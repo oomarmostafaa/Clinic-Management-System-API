47659e1d8d22a421f774263ccdbbea518537db16
oomarmostafaa <omarmo5252@gmil.com>
2026-05-22 14:58:13 +0300

    test

--- Databases/db.connection.js
+++ Databases/db.connection.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 export const dbConnection = () => {
+  console.log(process.env.DB_URL);
   mongoose
     .connect(process.env.DATABASE_URL)
     .then(() => console.log("DB connected"))
